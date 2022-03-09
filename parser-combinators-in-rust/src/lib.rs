92f7ca8c194ef5af0019603a102f5cec654d1d86
takumi ikeda <bshuge02@gmail.com>
2022-03-09 14:36:06 +0000

    add: parser character_function

--- parser-combinators-in-rust/src/lib.rs
+++ parser-combinators-in-rust/src/lib.rs
@@ -1,6 +1,3 @@
-assert_eq!(digits("123*456"), Some((123, "*456")));
-assert_eq!(digits("ABCDEFG"), None);
-
 // S の先頭にある整数をパースし、整数値と残りの文字列を返す。
 // パースに失敗した場合は None を返す。
 pub fn digits(s: &str) -> Option<(i64, &str)> {
@@ -8,5 +5,17 @@ pub fn digits(s: &str) -> Option<(i64, &str)> {
     match s[..end].parse() {
         Ok(value) => Some((value, &s[end..])),
         Err(_) => None,
+    }
+}
+
+// 先頭の一文字が c であるときに成功して　()　を返すようなパーサを返す。
+pub fn character(c: char) -> impl Fn(&str) -> Option<((), &str)> {
+    move |s| {
+        let mut chars = s.chars();
+        if chars.next() == Some(c) {
+            Some(((), chars.as_str()))
+        } else {
+            None
+        }
     }
 }
