31a54479d7a6e3da7a2b9ba6a6d5346b26ee206b
takumi ikeda <bshuge02@gmail.com>
2022-03-09 13:54:50 +0000

    add :perse baseoption

--- parser-combinators-in-rust/src/lib.rs
+++ parser-combinators-in-rust/src/lib.rs
@@ -1,8 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+assert_eq!(digits("123*456"), Some((123, "*456")));
+assert_eq!(digits("ABCDEFG"), None);
+
+// S の先頭にある整数をパースし、整数値と残りの文字列を返す。
+// パースに失敗した場合は None を返す。
+pub fn digits(s: &str) -> Option<(i64, &str)> {
+    let end = s.find(|c: char| !c.is_ascii_digit()).unwrap_or(s.len());
+    match s[..end].parse() {
+        Ok(value) => Some((value, &s[end..])),
+        Err(_) => None,
     }
 }
